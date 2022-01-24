f58822989f4776db7c999969d4f9e66d858dfb46
Sam Bencivengo <sambencivengo@gmail.com>
2022-01-24 08:33:29 -0500

    fixes 🤫

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
+require('dotenv').config();
 //middleware
 
 app.use(express.json());
@@ -22,4 +22,18 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, () => console.log(`server is listening on port ${port}`));
+const start = async () => {
+	// we are awaiting the promise from connectDB
+	// if the promise resolves, we connect to the database
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () =>
+			console.log(`server is listening on port ${port}`)
+		);
+	} catch (error) {
+		// if the promise is rejected, then we log an error
+		console.log(error);
+	}
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-	'mongodb+srv://sam:Brainwash692@nodeexpressprojects.ugvyv.mongodb.net/NODEJS-PROJECT?retryWrites=true&w=majority';
+const connectDB = (url) => {
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	});
+};
 
-// this returns a promise, so we can use.then()
-mongoose
-	.connect(connectionString)
-	.then(() => console.log('CONNECTED TO THE DB...'))
-	.catch((err) => console.log(err));
+module.exports = connectDB;
