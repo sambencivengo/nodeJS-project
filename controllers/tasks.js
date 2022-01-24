005759760ce67f488d1d889e7fc2211f6832aebc
Sam Bencivengo <sambencivengo@gmail.com>
2022-01-24 12:10:32 -0500

    setting up model/controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,36 @@
-const getAllTasks = (req, res) => {
-	res.send('get all tasks ');
+const Task = require('../models/task');
+
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
-const createTask = (req, res) => {
-	res.json(req.body);
+
+const createTask = async (req, res) => {
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
-const getTask = (req, res) => {
-	res.json(req.params.id);
+
+const getTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOne({ _id: taskID });
+
+		if(!task){
+			
+		}
+		res.status(200).json({ task });
+
+	} catch (error) {
+
+	}
 };
 const updateTask = (req, res) => {
 	res.send('task updated');
